de4e80534627616393800d34f48ec93230b9e3ba
Lee Obungen <lobungen@github.com>
2025-06-08 19:43:36 -0500

    debug server file to render

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 // Import the routes
@@ -9,8 +11,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -18,6 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
